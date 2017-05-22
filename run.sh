3c9117a79a40166c05316d5d027b7d3c5bebe196
Alexander Tumin <iamtakingiteasy@eientei.org>
2017-05-22 13:56:57 +0300

    Update run.sh + travis

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+docker swarm init &>/dev/null || true
+
+docker volume create --name data-mongo
+docker volume create --name data-prometheus
+docker volume create --name data-grafana
+docker volume create --name data-logging
+docker volume create --name data-elasticsearch
+
 docker-compose -p dclou -f docker-infra.yml up -d
 sleep 10
 docker-compose -p dclou -f docker-platform.yml up -d
