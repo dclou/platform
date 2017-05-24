c78fc0c9c5564eeb2dea9c55f5ffb9b3b4eea20a
vilkov.artem <vilkov.artem@otr.ru>
2017-05-24 14:37:17 +0500

    добавил автоматическое создание докер-машины

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-docker swarm init &>/dev/null || true
+docker-machine create -d virtualbox --virtualbox-cpu-count 4 --virtualbox-memory 7096 dclou || true
+eval $(docker-machine env dclou)
+
+export IP="$(docker-machine ip dclou)"
+docker swarm init --advertise-addr $IP &>/dev/null || true
 
 docker stack deploy -c docker-infra.yml dclou
 docker stack deploy -c docker-platform.yml dclou
