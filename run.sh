e1eafd1070494a6fc6a7bad98a1f40ff8c2f1d09
Alexander Tumin <iamtakingiteasy@eientei.org>
2017-05-24 15:39:48 +0300

    Appends to run.sh

--- run.sh
+++ run.sh
@@ -2,16 +2,22 @@
 
 BIP=127.0.0.1
 
-if command -v docker-machine &>/dev/null; then
-  docker-machine create -d virtualbox --virtualbox-cpu-count 4 --virtualbox-memory 7096 dclou || true
-  eval $(docker-machine env dclou)
-
-  export IP="$(docker-machine ip dclou)"
-  docker swarm init --advertise-addr $IP &>/dev/null || true
-  BIP=$IP
-else
-  docker swarm init &>/dev/null || true
+if [ $# -gt 0 ] && [ $1 = "init" ]; then
+  if command -v docker-machine &>/dev/null; then
+    docker-machine create -d virtualbox --virtualbox-cpu-count 4 --virtualbox-memory 7096 dclou || true
+    eval $(docker-machine env dclou)
+    docker swarm init --advertise-addr $IP &>/dev/null || true
+  else
+    docker swarm init &>/dev/null || true
+  fi      
 fi
+
+if command -v docker-machine &>/dev/null && [ -z "$IP" ]; then
+  eval $(docker-machine env dclou)
+  export IP="$(docker-machine ip dclou)"
+  BIP=$IP
+fi
+
 
 docker stack deploy -c docker-infra.yml dclou
 while ! curl -s http://${BIP}:8761 &>/dev/null; do sleep 2; echo Waiting for discovery server...; done &
