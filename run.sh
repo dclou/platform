c897689d7be544b17b022338267e356c50549309
Alexander Tumin <iamtakingiteasy@eientei.org>
2017-05-24 14:43:45 +0300

    Deploy

--- run.sh
+++ run.sh
@@ -6,6 +6,10 @@ export IP="$(docker-machine ip dclou)"
 docker swarm init --advertise-addr $IP &>/dev/null || true
 
 docker stack deploy -c docker-infra.yml dclou
+while ! curl -s http://127.0.0.1:8761 &>/dev/null; do sleep 2; echo Waiting for discovery server...; done &
+while ! curl -s http://127.0.0.1:8888 &>/dev/null; do sleep 2; echo Waiting for config server...; done &
+wait
 docker stack deploy -c docker-platform.yml dclou
-
-
+while ! curl -s http://127.0.0.1:7070 &>/dev/null; do sleep 2; echo Waiting for admin server...; done &
+while ! curl -s http://127.0.0.1:8080 &>/dev/null; do sleep 2; echo Waiting for gateway server...; done &
+wait
