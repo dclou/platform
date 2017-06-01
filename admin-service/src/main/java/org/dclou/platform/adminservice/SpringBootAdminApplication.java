eef20cf45fabf9b7cc9343108ab22a5cd7b7560f
vilkov.artem <vilkov.artem@otr.ru>
2017-06-01 12:03:53 +0500

    Добавлен дашбоард microservices-dashboard в admin-service Актуализирована версия spring-cloud

--- admin-service/src/main/java/org/dclou/platform/adminservice/SpringBootAdminApplication.java
+++ admin-service/src/main/java/org/dclou/platform/adminservice/SpringBootAdminApplication.java
@@ -3,9 +3,10 @@ package org.dclou.platform.adminservice;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-
+import be.ordina.msdashboard.EnableMicroservicesDashboardServer;
 
 @EnableAdminServer
+@EnableMicroservicesDashboardServer
 @SpringCloudApplication
 public class SpringBootAdminApplication {
 	public static void main(String[] args) {
