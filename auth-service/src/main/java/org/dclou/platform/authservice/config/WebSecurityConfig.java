ed624ab92a88898f2833cdcc337ed501fdec22dc
max <maxim.nikitin.work@gmail.com>
2017-06-06 20:31:11 +0300

    oauth2

---
+++ auth-service/src/main/java/org/dclou/platform/authservice/config/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package org.dclou.platform.authservice.config;
+
+import org.dclou.platform.authservice.Oauth2UserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by msnikitin on 24.04.2017.
+ */
+@Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private Oauth2UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login", "/oauth/token", "/oauth/check_token", "/health", "/metrics")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.parentAuthenticationManager(authenticationManager).userDetailsService(userDetailsService);
+    }
+}
