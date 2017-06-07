0add86a72fbd3e0b6e74519b6519719f34bef4a4
Alexander Tumin <iamtakingiteasy@eientei.org>
2017-06-07 13:05:55 +0300

    Hystrix dashboard

--- admin-service/src/main/java/org/dclou/platform/adminservice/SpringBootAdminApplication.java
+++ admin-service/src/main/java/org/dclou/platform/adminservice/SpringBootAdminApplication.java
@@ -1,11 +1,13 @@
 package org.dclou.platform.adminservice;
 
+import be.ordina.msdashboard.EnableMicroservicesDashboardServer;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import be.ordina.msdashboard.EnableMicroservicesDashboardServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+@EnableHystrixDashboard
 @EnableTurbine
 @EnableAdminServer
 @EnableMicroservicesDashboardServer
