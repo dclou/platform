3fa1d60ae38917bedfe21557af18d5c28ab4e0d6
Alexander Tumin <iamtakingiteasy@eientei.org>
2017-06-11 15:46:18 +0300

    Admin pull

--- admin-service/src/main/java/org/dclou/platform/adminservice/SpringBootAdminApplication.java
+++ admin-service/src/main/java/org/dclou/platform/adminservice/SpringBootAdminApplication.java
@@ -1,18 +1,33 @@
 package org.dclou.platform.adminservice;
 
 import be.ordina.msdashboard.EnableMicroservicesDashboardServer;
+import com.netflix.turbine.streaming.servlet.TurbineStreamServlet;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.TurbineHttpConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @EnableHystrixDashboard
-@EnableTurbine
 @EnableAdminServer
 @EnableMicroservicesDashboardServer
 @SpringCloudApplication
-public class SpringBootAdminApplication {
+@EnableDiscoveryClient
+@EnableConfigurationProperties
+@EnableTurbine
+public class SpringBootAdminApplication extends TurbineHttpConfiguration {
+
+	@Bean
+	@Override
+	public ServletRegistrationBean turbineStreamServlet() {
+		return new ServletRegistrationBean(new TurbineStreamServlet(), "/admin/api/turbine/stream");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAdminApplication.class, args);
 	}
