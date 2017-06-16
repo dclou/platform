23e50d4df1863e0c6903f359cce1d955b8ae5275
Alexander Tumin <iamtakingiteasy@eientei.org>
2017-06-16 11:28:17 +0300

    Deploy-clean

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,6 +6,10 @@ ssh user@$HOST 'ssh user@10.10.50.11 docker stack rm dclou'
 
 mvn -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -s .travis.settings.xml -B deploy -DdeployDocker
 
+ssh user@$HOST 'ssh user@10.10.50.11 "docker images --format {{.ID}} | xargs docker rmi -f"'
+ssh user@$HOST 'ssh user@10.10.50.21 "docker images --format {{.ID}} | xargs docker rmi -f"'
+ssh user@$HOST 'ssh user@10.10.50.22 "docker images --format {{.ID}} | xargs docker rmi -f"'
+
 ssh user@$HOST 'ssh user@10.10.50.11 rm -rf platform demogpb'
 ssh user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/platform'
 ssh user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/demogpb'
