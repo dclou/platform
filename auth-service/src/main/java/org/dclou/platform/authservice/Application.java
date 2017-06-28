9c8485021da0af50f21cb696915894ac0e266b1c
max <maxim.nikitin.work@gmail.com>
2017-06-28 14:33:54 +0300

    google recaptcha

--- auth-service/src/main/java/org/dclou/platform/authservice/Application.java
+++ auth-service/src/main/java/org/dclou/platform/authservice/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
 @SpringCloudApplication
-public class AuthServiceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
