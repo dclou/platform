f4f92cf278766f67673d6ece00572998a956fa4d
Alexander Tumin <iamtakingiteasy@eientei.org>
2017-06-30 16:40:20 +0300

    Check

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,17 +2,16 @@
 
 HOST="$1"
 
-ssh-keyscan -H $HOST > ~/.ssh/known_hosts
-ssh -p50022 user@$HOST 'ssh user@10.10.50.11 docker stack rm dclou'
+ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 docker stack rm dclou'
 
-mvn -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -s .travis.settings.xml -B deploy -DdeployDocker
+mvn -oStrictHostKeyChecking=no -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -s .travis.settings.xml -B deploy -DdeployDocker
 
-ssh -p50022 user@$HOST 'ssh user@10.10.50.11 "docker images --format {{.ID}} | xargs docker rmi -f"'
-ssh -p50022 user@$HOST 'ssh user@10.10.50.21 "docker images --format {{.ID}} | xargs docker rmi -f"'
-ssh -p50022 user@$HOST 'ssh user@10.10.50.22 "docker images --format {{.ID}} | xargs docker rmi -f"'
+ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 "docker images --format {{.ID}} | xargs docker rmi -f"'
+ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.21 "docker images --format {{.ID}} | xargs docker rmi -f"'
+ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.22 "docker images --format {{.ID}} | xargs docker rmi -f"'
 
-ssh -p50022 user@$HOST 'ssh user@10.10.50.11 rm -rf platform demogpb'
-ssh -p50022 user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/platform'
-ssh -p50022 user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/demogpb'
-ssh -p50022 user@$HOST 'ssh user@10.10.50.11 "cd platform && ./run.sh"'
-ssh -p50022 user@$HOST 'ssh user@10.10.50.11 "cd demogpb && ./run.sh"'
+ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 rm -rf platform demogpb'
+ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/platform'
+ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/demogpb'
+ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 "cd platform && ./run.sh"'
+ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 "cd demogpb && ./run.sh"'
