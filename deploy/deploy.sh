3273f8ed9328f0cccece9a7dca271957371697b0
Alexander Tumin <iamtakingiteasy@eientei.org>
2017-06-30 15:26:17 +0300

    Fixed ssh ports

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,16 +2,16 @@
 
 HOST="$1"
 
-ssh user@$HOST 'ssh -p50022 user@10.10.50.11 docker stack rm dclou'
+ssh -p50022 user@$HOST 'ssh user@10.10.50.11 docker stack rm dclou'
 
 mvn -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -s .travis.settings.xml -B deploy -DdeployDocker
 
-ssh user@$HOST 'ssh user@10.10.50.11 "docker images --format {{.ID}} | xargs docker rmi -f"'
-ssh user@$HOST 'ssh user@10.10.50.21 "docker images --format {{.ID}} | xargs docker rmi -f"'
-ssh user@$HOST 'ssh user@10.10.50.22 "docker images --format {{.ID}} | xargs docker rmi -f"'
+ssh -p50022 user@$HOST 'ssh user@10.10.50.11 "docker images --format {{.ID}} | xargs docker rmi -f"'
+ssh -p50022 user@$HOST 'ssh user@10.10.50.21 "docker images --format {{.ID}} | xargs docker rmi -f"'
+ssh -p50022 user@$HOST 'ssh user@10.10.50.22 "docker images --format {{.ID}} | xargs docker rmi -f"'
 
-ssh user@$HOST 'ssh user@10.10.50.11 rm -rf platform demogpb'
-ssh user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/platform'
-ssh user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/demogpb'
-ssh user@$HOST 'ssh user@10.10.50.11 "cd platform && ./run.sh"'
-ssh user@$HOST 'ssh user@10.10.50.11 "cd demogpb && ./run.sh"'
+ssh -p50022 user@$HOST 'ssh user@10.10.50.11 rm -rf platform demogpb'
+ssh -p50022 user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/platform'
+ssh -p50022 user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/demogpb'
+ssh -p50022 user@$HOST 'ssh user@10.10.50.11 "cd platform && ./run.sh"'
+ssh -p50022 user@$HOST 'ssh user@10.10.50.11 "cd demogpb && ./run.sh"'
