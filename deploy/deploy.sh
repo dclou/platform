d5c224cbe66c1227cb52c27600519883d7b97a8c
Alexander Tumin <iamtakingiteasy@eientei.org>
2017-07-02 17:16:55 +0300

    True

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,9 +8,9 @@ ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 docker s
 
 mvn -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -s .travis.settings.xml -B deploy -DdeployDocker
 
-ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 "docker images --format {{.ID}} | xargs docker rmi -f"'
-ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.21 "docker images --format {{.ID}} | xargs docker rmi -f"'
-ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.22 "docker images --format {{.ID}} | xargs docker rmi -f"'
+ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 "docker images --format {{.ID}} | xargs docker rmi -f"' || true
+ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.21 "docker images --format {{.ID}} | xargs docker rmi -f"' || true
+ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.22 "docker images --format {{.ID}} | xargs docker rmi -f"' || true
 
 ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 rm -rf platform demogpb'
 ssh -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/platform'
